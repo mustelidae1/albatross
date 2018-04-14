4f96a4de76911b89a760c30f9da05f09c34393be
CMRoss <ChrisRoss@u.boisestate.edu>
2018-04-13 19:02:22 -0600

    Working Rock Waves V1
    rocks go marching two by two....

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -16,10 +16,10 @@ public class GameController : MonoBehaviour {
     public float startWait;
     public float waveWait;
 
-    public Text restartText;
-    public Text gameOverText;
-    public Text scoreText;
-    private int score;
+    //public Text restartText;
+    //public Text gameOverText;
+    //public Text scoreText;
+    //private int score;
 
     //vars to check for to restart and when game is over
     private bool gameOver;
@@ -33,11 +33,11 @@ public class GameController : MonoBehaviour {
         restart = false;
 
         //labels will diplay nothing
-        restartText.text = "";
-        gameOverText.text = "";
+        //restartText.text = "";
+        //gameOverText.text = "";
 
-        score = 0;
-        UpdateScore();
+        //score = 0;
+        //UpdateScore();
 
         //SpawnWaves();  <---not co-routine
         StartCoroutine (SpawnWaves());
@@ -55,10 +55,13 @@ public class GameController : MonoBehaviour {
             for (int i = 0; i < hazardCount; i++)
             {
                 //random.range (min, max) values for asteroid spawn point on X axis
-                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Vector3 spawnPosition1 = new Vector3(spawnValues.x, Random.Range(spawnValues.y-3, spawnValues.y+3), spawnValues.z);
+                Vector3 spawnPosition2 = new Vector3(spawnValues.x+35, Random.Range(spawnValues.y - 8, spawnValues.y), spawnValues.z-15);
                 //Quaternion.identity cooresponds to no rotation at all
-                Quaternion spawnRotation = Quaternion.identity;
-                Instantiate(hazard, spawnPosition, spawnRotation);
+                Quaternion spawnRotation = Quaternion.Euler(0,90,0);
+                
+                Instantiate(hazard, spawnPosition1, spawnRotation);
+                Instantiate(hazard, spawnPosition2, spawnRotation);
 
                 //this function needs to be a coroutine to work with this "waitForSeconds" thing
                 //WaitForSeconds(spawnWait);   <----won't work
@@ -70,42 +73,42 @@ public class GameController : MonoBehaviour {
 
             yield return new WaitForSeconds(waveWait);
 
-            if (gameOver)
-            {
-                restartText.text = "Press 'R' to Restart";
-                restart = true;
-                break;
-            }
+            //if (gameOver)
+            //{
+            //    restartText.text = "Press 'R' to Restart";
+            //    restart = true;
+            //    break;
+            //}
         }
     }
 
-    private void Update()
-    {
-        //if restart equals true...
-        if (restart)
-        {
-            //if R is pressed
-            if (Input.GetKeyDown (KeyCode.R))
-            {
-                SceneManager.LoadScene("Main");
-            }
-        }
-    }
+    //private void Update()
+    //{
+    //    //if restart equals true...
+    //    if (restart)
+    //    {
+    //        //if R is pressed
+    //        if (Input.GetKeyDown (KeyCode.R))
+    //        {
+    //            SceneManager.LoadScene("Main");
+    //        }
+    //    }
+    //}
 
-    public void AddScore(int newScoreValue)
-    {
-        score += newScoreValue;
-        UpdateScore();
-    }
+    //public void AddScore(int newScoreValue)
+    //{
+    //    score += newScoreValue;
+    //    UpdateScore();
+    //}
 
-    public void GameOver()
-    {
-        gameOverText.text = "Game Over!";
-        gameOver = true;
-    }
+    //public void GameOver()
+    //{
+    //    gameOverText.text = "Game Over!";
+    //    gameOver = true;
+    //}
 
-    void UpdateScore()
-    {
-        scoreText.text = "Score: " + score.ToString();
-    }
+    //void UpdateScore()
+    //{
+    //    scoreText.text = "Score: " + score.ToString();
+    //}
 }
---
+++ Assets/__Scripts/Wind.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+    public float windSpeed;
+    private void OnTriggerEnter(Collider coll)
+    {
+        print("Enter OnTriggerEnter");
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith.tag == "Player")
+        {
+            collidedWith.GetComponent<Rigidbody>().AddForce(Vector3.up * windSpeed);
+        }
+    }
+}
